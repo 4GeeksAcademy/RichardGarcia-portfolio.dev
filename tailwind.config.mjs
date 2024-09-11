4e0be76336cd16b33d9465d79baccebc95f98753
Cecilia Perdomo <83676442+CeciliaBPerdomo@users.noreply.github.com>
2024-09-11 21:17:05 +0000

    prueba deploy

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	darkMode: 'class',
+// export default {
+// 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+// 	darkMode: 'class',
+// 	theme: {
+// 		extend: {},
+// 	},
+// 	plugins: [],
+// }
+
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx}'],
 	theme: {
-		extend: {},
+	  extend: {},
 	},
 	plugins: [],
-}
+  };
+  
